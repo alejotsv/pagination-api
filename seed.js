5cf5f57a30c9be3c7b7f9119234456249397a222
Alejandro Salas <asalas.wd@outlook.com>
2020-01-19 10:15:43 -0500

    store function to create seed

---
+++ seed.js
@@ -0,0 +1,22 @@
+// Function to create seed
+// function createApps(num) {
+//   let seed = [];
+//   let id = '';
+//   let name = '';  
+//   for (let i=1; i<=num; i++){
+//     id = i;
+//     console.log(id);
+//     if (i<10){
+//       name = 'my-app-00' + id;  
+//     } else if (i<100){
+//       name = 'my-app-0' + id;
+//     } else {
+//       name = 'my-app-' + id;
+//     }
+//     console.log(name);
+//     seed.push({'id': id, 'name': name})    
+//   }
+//   console.log(seed);
+// }
+
+// createApps(10);
