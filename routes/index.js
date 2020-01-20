edbfb0d2c44d4ff0317e55aed65de3bfcfb0cd10
Alejandro Salas <asalas.wd@outlook.com>
2020-01-20 07:15:07 -0500

    verify if the required field 'by' exists

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,18 @@ const MyApp = require('../models/MyApp');
 /* GET home page */
 router.get('/apps', (req, res, next) => {  
   let by;
+  if (req.body.range.by !== 'id' && req.body.range.by !== 'name'){
+    res.send(`The field 'by' is required and valid values are either 'id' or 'name'`);
+  } else {
+    by = req.body.range.by;
+  }
   let start;
   let end;
   let max;
   let order; 
   console.log(req.body);
-  console.log(req.body.range.by);
-
+  console.log('this is the by: ' + by);
+  
   MyApp
     .find()    
     .then( apps => {
