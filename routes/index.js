bc56834435b6bdcff4fa0003bf3e4638779562a7
Alejandro Salas <asalas.wd@outlook.com>
2020-01-20 19:46:24 -0500

    define limit

--- routes/index.js
+++ routes/index.js
@@ -17,23 +17,29 @@ router.get('/apps', (req, res, next) => {
 
   // Verify if the field 'start' exists and set value or default to variable
   if (!req.body.range.start) {
-    start = 0;
+    start = 1;
   } else if (by==='id'){
     start = req.body.range.start;
   } else if (by==='name') {
+    // remove 'my-app-' from app name and convert to number
     start = Number(req.body.range.start.split('-').pop());
   }
 
   console.log('this is start: ' + start);
   
-  let end;  
+  let end;
 
-   // Verify if the field 'end' exists and set value or default to variable
-   if (req.body.range.start) {
-    end = req.body.range.end;
+  // Verify if the field 'end' exists and set value or default to variable
+  if (req.body.range.end) {
+   if (by==='id'){
+     end = req.body.range.end - start + 1;
+   } else if (by==='name'){
+     // remove 'my-app-' from app name and convert to number
+     end = Number(req.body.range.end.split('-').pop()) - start + 1;
+   }
   }
 
-  let max;
+  let max;  
 
   // Verify if the field 'max' exists and set value or default to variable
   if (!req.body.range.max) {
@@ -42,14 +48,23 @@ router.get('/apps', (req, res, next) => {
     max = req.body.range.max;
   }
 
+  let limit;
+
+  // Verify if limit will match end or max
+  if (!req.body.range.end || end > max) {
+    limit = max;
+  } else {
+    limit = end;
+  }
+
   let order; 
-  console.log(req.body);
-  console.log(`this is max: ${max}`);
+  console.log(req.body);   
   
   
   MyApp
     .find()
     .select('-_id')
+    .limit(limit)
     .skip(start-1)
     .then( apps => {
       // TODO: create an array of pages according to the specifications and return that      
